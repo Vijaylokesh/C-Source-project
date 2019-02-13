c3afac6b0ac627060fc1d1a4be2e67efa4ad23e5
Vijaylokesh <47449185+Vijaylokesh@users.noreply.github.com>
2019-02-13 22:38:24 +0530

    Update jenkinsBackup.sh

--- jenkinsBackup.sh
+++ jenkinsBackup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 pwd
 date=`date +'%d-%m-%Y_%H:%M'`
-sudo cp  /var/lib/jenkins/workspace/vijay/ABCD.exe /var/lib/jenkins/workspace/vijay/ABCD_$date.exe
-sudo cp -p /var/lib/jenkins/workspace/Build1/ABCD_$date.exe /home/ec2-user/jenkinsBackup/
+cp  /var/lib/jenkins/workspace/vijay/ABCD.exe /var/lib/jenkins/workspace/vijay/ABCD_$date.exe
+cp -p /var/lib/jenkins/workspace/Build1/ABCD_$date.exe /home/ec2-user/jenkinsBackup/
 make clean
-sudo rm -rf ABCD_$date.exe ABCD.exe
-sudo ls /home/ec2-user/jenkinsBackup/
+ls /home/ec2-user/jenkinsBackup/

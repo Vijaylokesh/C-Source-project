44f4682a62653d443f43b85c4a8b5398f79c30cf
Vijaylokesh <47449185+Vijaylokesh@users.noreply.github.com>
2019-02-14 22:38:49 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@ pwd
 date=`date +'%d-%m-%y_%H:%m'`
 #path=`sudo find / iname "ABCD.exe" | ls -lrt |tail -1 |awk -F " " '{print $NF}'`
 path=`pwd`
-sudo cp $path/ABCD.exe $path/ABCD.exe_$date.exe
+sudo cp $path/ABCD.exe $path/ABCD_$date.exe
 sudo cp -p $path/ABCD_$date.exe /home/ec2-user/jenkins
 make clean
 sudo rm -rf ABCD_$date.exe ABCD.exe

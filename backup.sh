dfec5cd6df732962c702d961c16df2b5f5a5bc59
Vijaylokesh <47449185+Vijaylokesh@users.noreply.github.com>
2019-02-14 20:43:49 +0530

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+pwd
+date=`date +'%d-%m-%y_%H:%m'`
+#path=`sudo find / iname "ABCD.exe" | ls -lrt |tail -1 |awk -F " " '{print $NF}'`
+path=`pwd`
+sudo cp $path/ABCD.exe $path/ABCD.exe_$date.exe
+sudo cp -p $path/ABCD.exe_$date.exe /home/ec2-user/jenkinsBackup
+make clean
+sudo rm -rf ABCD_$date.exe ABCD.exe
